20e9c32e7dabec84dad93642268fecc9a26311b8
Sanjula Madushanka <166517153+Sanjula-Madushanka@users.noreply.github.com>
2025-12-19 17:21:26 +0530

    updated

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ const app = express();
 // Middleware
 // ====================
 app.use(cors({
-  origin: "http://localhost:3000", // React frontend
+  origin: "http://localhost:3000", origin: ["http://localhost:5173", "http://localhost:3000"],
   credentials: true
 }));
 app.use(express.json());
