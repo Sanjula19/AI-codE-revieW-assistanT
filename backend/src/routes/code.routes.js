8b663b6408ee7a32df7acdb53d3876edcf46b1b1
Sanjula Madushanka <166517153+Sanjula-Madushanka@users.noreply.github.com>
2025-12-19 20:50:36 +0530

    Updated

--- backend/src/routes/code.routes.js
+++ backend/src/routes/code.routes.js
@@ -1,25 +1,9 @@
 // src/routes/code.routes.js
-const { authJwt } = require("../middlewares");
+const express = require("express");
+const router = express.Router();
 const controller = require("../controllers/code.controller");
-const historyCtrl = require("../controllers/history.controller");
 
-module.exports = function(app) {
-  app.use((req, res, next) => {
-    res.setHeader("Access-Control-Allow-Headers", "x-access-token, Content-Type");
-    next();
-  });
+// Route: POST /api/code/upload
+router.post("/upload", controller.uploadCode);
 
-  // Upload code
-  app.post(
-    "/api/code/upload",
-    [authJwt.verifyToken],
-    controller.uploadCode
-  );
-
-  // Get history
-  app.get(
-    "/api/code/history",
-    [authJwt.verifyToken],
-    historyCtrl.getHistory
-  );
-};
+module.exports = router;
